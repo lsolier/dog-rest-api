68c5070c59cab5386460dc5255adcc8aacf4f1a2
Luis Solier Sajami <luis.sajami@gmail.com>
2019-10-08 17:10:21 -0500

    add data layer

--- src/main/java/com/lsolier/udacity/dog/entity/Dog.java
+++ src/main/java/com/lsolier/udacity/dog/entity/Dog.java
@@ -1,4 +1,24 @@
 package com.lsolier.udacity.dog.entity;
 
-public class dog {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Dog {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String name;
+  private String breed;
+  private String origin;
+
 }
--- src/main/java/com/lsolier/udacity/dog/repository/DogRepository.java
+++ src/main/java/com/lsolier/udacity/dog/repository/DogRepository.java
@@ -1,4 +1,26 @@
 package com.lsolier.udacity.dog.repository;
 
-public interface DogRepository {
+import com.lsolier.udacity.dog.entity.Dog;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DogRepository extends CrudRepository<Dog, Long> {
+
+  String FIND_BREEDS = "Select d.id, d.breed from dog d";
+  @Query(value = FIND_BREEDS, nativeQuery = true)
+  List<String> findAllBreeds();
+
+  String FIND_NAMES = "Select d.id, d.name from dog d";
+  @Query(value = FIND_NAMES, nativeQuery = true)
+  List<String> findAllNames();
+
+  String FIND_BREED_BY_ID = "Select d.id, d.breed from dog d where d.id = :id";
+  @Query(value = FIND_BREED_BY_ID, nativeQuery = true)
+  String findBreedById(@Param("id") Long Id);
+
 }
