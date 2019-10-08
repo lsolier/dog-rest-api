3eb99396594586f7013e0b0ebc9cd0741913af17
Luis Solier Sajami <luis.sajami@gmail.com>
2019-10-08 17:10:48 -0500

    add service layer

--- src/main/java/com/lsolier/udacity/dog/service/DogService.java
+++ src/main/java/com/lsolier/udacity/dog/service/DogService.java
@@ -1,4 +1,16 @@
 package com.lsolier.udacity.dog.service;
 
+import com.lsolier.udacity.dog.entity.Dog;
+
+import java.util.List;
+
 public interface DogService {
+
+  List<String> retrieveDogBreed();
+
+  String retrieveDogBreedById(Long id);
+
+  List<String> retrieveDogNames();
+
+  List<Dog> retrieveDogs();
 }
--- src/main/java/com/lsolier/udacity/dog/service/impl/DogServiceImpl.java
+++ src/main/java/com/lsolier/udacity/dog/service/impl/DogServiceImpl.java
@@ -1,4 +1,44 @@
 package com.lsolier.udacity.dog.service.impl;
 
-public class DogServiceImpl {
+import com.lsolier.udacity.dog.entity.Dog;
+import com.lsolier.udacity.dog.repository.DogRepository;
+import com.lsolier.udacity.dog.service.DogNotFoundException;
+import com.lsolier.udacity.dog.service.DogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DogServiceImpl implements DogService {
+
+  private DogRepository dogRepository;
+
+  @Autowired
+  public DogServiceImpl(DogRepository dogRepository) {
+    this.dogRepository = dogRepository;
+  }
+
+  @Override
+  public List<String> retrieveDogBreed() {
+    return dogRepository.findAllBreeds();
+  }
+
+  @Override
+  public String retrieveDogBreedById(Long id) {
+    Optional<String> optionalBreed = Optional.ofNullable(dogRepository.findBreedById(id));
+    String breed = optionalBreed.orElseThrow(DogNotFoundException::new);
+    return breed;
+  }
+
+  @Override
+  public List<String> retrieveDogNames() {
+    return dogRepository.findAllNames();
+  }
+
+  @Override
+  public List<Dog> retrieveDogs() {
+    return (List<Dog>) dogRepository.findAll();
+  }
 }
