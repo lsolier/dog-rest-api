d95578a2fa1b88c82347dd9acb5ff647a48a9bef
Luis Solier Sajami <luis.sajami@gmail.com>
2019-10-08 17:11:02 -0500

    add data sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO dog (id, name, breed, origin) VALUES (1, 'Leonardo','Pastor Aleman','Alemania');
+INSERT INTO dog (id, name, breed, origin) VALUES (2, 'Raphael','Husky Siberiano','Siberia');
+INSERT INTO dog (id, name, breed, origin) VALUES (3, 'Donatello','San Bernardo','Italia');
+INSERT INTO dog (id, name, breed, origin) VALUES (4, 'Miguel Ángel','Akita Inu','Japon');
+INSERT INTO dog (id, name, breed, origin) VALUES (5, 'Splinter','Labrador Retriever','Isla de Terranova');
