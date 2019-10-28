2bd7d46930ac1c8d29304f6df98d4910ad45a018
Luis Solier Sajami <luis.sajami@gmail.com>
2019-10-27 21:21:59 -0500

    fix docs

--- src/main/java/com/lsolier/udacity/dog/config/SwaggerConfig.java
+++ src/main/java/com/lsolier/udacity/dog/config/SwaggerConfig.java
@@ -19,6 +19,7 @@ public class SwaggerConfig {
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(apiInfo())
         .select()
         .apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.any()).build()
